c64e13931ad7e87e9b8341fae5dfab0425584934
Oskitaa <ese.betico.98@gmail.com>
2020-09-28 09:35:07 +0200

    nuevos ejercicios iniciacion

--- Cliente/Ejercicios iniciacion/ejercicio15.js
+++ Cliente/Ejercicios iniciacion/ejercicio15.js
@@ -4,14 +4,12 @@ window.onload = () => {
 	document.onclick = ()=> {document.getElementById('info').style.backgroundColor = '#FFFFCC'};
 }
 
-function capturarRaton(evento){
-	var eve = evento || window.event;
+function capturarRaton(eve){
 	muestraInformacion(['Ratón',`Navegador [${eve.screenX}, ${eve.screenY}]`,`Pagina [${eve.clientX}, ${eve.clientY}]`])
 	document.getElementById('info').style.backgroundColor = '#FFFFFF';
 }
 
-function capturarTeclado(evento){
-	var eve = evento || window.event;
+function capturarTeclado(eve){
 	muestraInformacion(['Teclado',`Carácter [${String.fromCharCode(eve.keyCode)}]`,`Código [${eve.keyCode}]`])
 	document.getElementById('info').style.backgroundColor = '#CCE6FF';
 }
@@ -23,19 +21,24 @@ function muestraInformacion(mensaje) {
 	}
 }
 
-var m = document.getElementById('info');
-m.addEventListener('mousedown', mouseDown, false);
-window.addEventListener('mouseup', mouseUp, false);
 
-function mouseUp() {
-    window.removeEventListener('mousemove', move, true);
+var div = document.getElementById('info');
+
+div.addEventListener('mousedown', down,false);
+window.addEventListener('mouseup', up, false)
+
+
+function down(){
+	window.addEventListener('mousemove', move);
 }
 
-function mouseDown(e) {
-    window.addEventListener('mousemove', move, true);
+function up(){
+	window.removeEventListener('mousemove', move);
 }
 
-function move(e) {
-    m.style.top = e.clientY + 'px';
-    m.style.left = e.clientX + 'px';
-};
+function move(e){
+	console.log('entra')
+	div.style.top = e.clientY + 'px';
+	div.style.left = e.clientX + 'px';
+
+}
