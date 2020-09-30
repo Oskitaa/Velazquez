dc3169250127b5f5a721294bf0479b80015654bd
Oskitaa <ese.betico.98@gmail.com>
2020-09-30 13:26:04 +0200

    fixed bug ejercicio validacion formulario (soloLetras)

--- Cliente/Ejercicios iniciacion/ejercicioValidacion.js
+++ Cliente/Ejercicios iniciacion/ejercicioValidacion.js
@@ -1,39 +1,37 @@
 document.getElementById("nombre").onkeypress = soloLetras;
 document.getElementById("enviar").onclick = validarTodo;
 
-
 function soloLetras(e) {
-    var letras = /[á-zÁ-ZñÑ ]/;
+    var letras = /[a-zA-ZñÑáéíúóÁÉÍÓÚ ]/;
     return letras.test(e.key);
 }
 
-
 function validarTodo() {
-        
-        var name = document.getElementById("nombre").value;
-        var info_name =document.getElementById('info_nombre');
-        (name.trim().length < 2) ?  info_name.innerHTML = 'Demasiado corto' : info_name.innerHTML = '';
 
-        var sex = document.getElementById('sexo').value;
-        var info_sexo =document.getElementById('info_sexo');
-        (sex == "") ? info_sexo.innerHTML = 'Seleccione una opción' :info_sexo.innerHTML = '';
+    var name = document.getElementById("nombre").value;
+    var info_name = document.getElementById('info_nombre');
+    (name.trim().length < 2) ? info_name.innerHTML = 'Demasiado corto' : info_name.innerHTML = '';
 
-        var dni = document.getElementById('dni').value;
-        var info_dni =document.getElementById('info_dni');
-        (!validaDni(dni.toUpperCase())) ? info_dni.innerHTML = 'DNI incorrecto' : info_dni.innerHTML = '';
+    var sex = document.getElementById('sexo').value;
+    var info_sexo = document.getElementById('info_sexo');
+    (sex == "") ? info_sexo.innerHTML = 'Seleccione una opción' : info_sexo.innerHTML = '';
 
-        var sug = document.getElementById('sugerencia').value;
-        var info_sugerencia =document.getElementById('info_sugerencia');
-        (sug.trim() == '') ? info_sugerencia.innerHTML = 'Obligatorio poner una sugerencia' : info_sugerencia.innerHTML = '';
+    var dni = document.getElementById('dni').value;
+    var info_dni = document.getElementById('info_dni');
+    (!validaDni(dni.toUpperCase())) ? info_dni.innerHTML = 'DNI incorrecto' : info_dni.innerHTML = '';
 
-        return (info_name.innerHTML == '' && info_sexo.innerHTML == '' && info_dni == '' && info_sugerencia == '') ? true : false;
+    var sug = document.getElementById('sugerencia').value;
+    var info_sugerencia = document.getElementById('info_sugerencia');
+    (sug.trim() == '') ? info_sugerencia.innerHTML = 'Obligatorio poner una sugerencia' : info_sugerencia.innerHTML = '';
+
+    return (info_name.innerHTML == '' && info_sexo.innerHTML == '' && info_dni.innerHTML == '' && info_sugerencia.innerHTML == '') ? true : false;
 }
 
 function validaDni(dni) {
 
     var numero, letr
     var letras = 'TRWAGMYFPDXBNJZSQVHLCKET';
-    var patternDni = /^\d{8}[a-zA-Z]$/;
+    var patternDni = /^\d{8}[A-Z]$/;
 
     if (patternDni.test(dni)) {
         numero = dni.substr(0, dni.length - 1) % 23;
